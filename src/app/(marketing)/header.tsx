f119e4bff4039ada1bdb0b1edc568ed3ae65ae97
Clinton <135718431+clix-M@users.noreply.github.com>
2024-03-16 17:18:05 -0500

    Agregado Marketing Page y  Autentification

---
+++ src/app/(marketing)/header.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+
+const Header = () => {
+    return ( 
+        <header className="h-20 w-full border-b-2 border-x-slate-200 px-4">
+            <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src={'/mascot.svg'} height={40} width={40} alt="Mascot" />
+                    <h1 className="text-2xl font-semibold text-green-600 tracking-wide">
+                        Clix Lingo
+                    </h1>
+                </div>
+
+                <ClerkLoading>
+                    <Loader className="w-5 h-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                    {/*
+                    if signed in, show the user button
+                     */}
+                    <SignedIn> 
+                        <UserButton 
+                        afterSignOutUrl="/"
+                        />
+                    </SignedIn>
+                    {/* 
+                    if not signed in, show the sign in button
+                     */}
+                    <SignedOut>
+                        <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn" >
+                            <Button variant={'ghost'}>
+                                Login
+                            </Button>
+                        </SignInButton>
+                    </SignedOut>
+                </ClerkLoaded>
+            </div>
+        </header>
+     );
+}
+ 
+export default Header;
