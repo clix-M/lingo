abe09fdf813e2b6a7efbbb0bd2f87376eb2e972a
Clinton <135718431+clix-M@users.noreply.github.com>
2024-03-18 08:54:25 -0500

    Actualizar diseño de la página de aprendizaje [Learn Page Wrappers]

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,8 +1,26 @@
 
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
     return ( 
-        <div>
-            Learning page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            {/* para que se vea los avances del curso */}
+            <StickyWrapper>
+                <UserProgress 
+                    activeCourse={{ title: "Spanish", imageSrc:"/es.svg"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+
+            {/* para que se pueda hacer el scroll de la pagina */}
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
         </div>
      );
 }
